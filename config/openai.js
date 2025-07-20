fb4d5932a6216d5cca94fadb7416b2b7f6b6f026
stanley <stanleyjo41@gmail.com>
2025-07-21 00:44:04 +0700

    Initial Commit

--- config/openai.js
+++ config/openai.js
@@ -4,7 +4,7 @@
 const { OpenAI } = require('openai');
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, 
+  apiKey: "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", 
 });
 
 console.log('✅ OpenAI client siap digunakan.');
