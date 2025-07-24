d323ff156c53f31b3e41dc3f80b3d29bbe4ff6e6
StanLee003 <86097005+StanLee003@users.noreply.github.com>
2025-07-24 22:41:56 +0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,8 +4,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/register-profile', authController.registerProfile);
-
 router.post('/check-username', authController.checkUsername);
 
 module.exports = router;
